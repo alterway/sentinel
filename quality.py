a063c31007a6dd9381054675e43546464e90b159
Ophélie MAUGER <ophelie.mauger@alterway.fr>
2018-08-01 09:55:57 +0200

    Improve quality pipeline

--- quality.py
+++ quality.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import subprocess
-import sys
-import logging
-
-
-def add_coloring_to_emit_ansi(fn):
-
-    def new(*args):
-        levelno = args[1].levelno
-        if levelno == 100:
-            color = '\x1b[36m'
-        elif levelno >= 50:
-            color = '\x1b[31m'
-        elif levelno >= 40:
-            color = '\x1b[31m'
-        elif levelno >= 30:
-            color = '\x1b[33m'
-        elif levelno >= 20:
-            color = '\x1b[32m'
-        elif levelno >= 10:
-            color = '\x1b[90m'
-        else:
-            color = '\x1b[0m'
-        args[1].msg = color + str(args[1].msg) + '\x1b[0m'
-
-        return fn(*args)
-    return new
-
+import os, sys, logging, subprocess, coloredlogs, verboselogs
 
 def run(command, chdir, action, silent=True, shell=True):
     errors = 0
-    logging.info('\nRunning %s' % action)
+    logger = logging.getLogger(__name__)
+    logger.debug('\nRunning %s' % action)
 
-    logging.info('    %s' % command)
+    logger.debug('    %s' % command)
     process = subprocess.Popen(
         command,
         cwd=chdir,
@@ -42,39 +16,30 @@ def run(command, chdir, action, silent=True, shell=True):
         bufsize=0,
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
+    output = ''
+    output, _ = process.communicate()
+
     if not silent:
-        output = ''
-
-        while True:
-            char = process.stdout.read(1)
-
-            if type(char) is bytes:
-                char = char.decode("utf-8", "replace")
-
-            if char == '' and process.poll() is not None:
-                break
-            if char != '':
-                output += char
-
-        logging.debug(output)
-    else:
-        output, error = process.communicate()
+        logger.notice(output.decode('utf-8'))
 
     if process.returncode == 0:
-        logging.info('    %s OK' % action)
+        logger.success('    %s OK' % action)
     else:
         if type(output) is bytes:
             output = output.decode("utf-8", "replace")
-        logging.warn('\n'.join(output.splitlines()))
-        logging.error('    %s FAILED' % action)
+        logger.warn('\n'.join(output.splitlines()))
+        logger.critical('    %s FAILED' % action)
         errors += 1
     return errors
 
 
 if __name__ == '__main__':
     FORMAT = '%(message)s'
-    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-    logging.StreamHandler.emit = add_coloring_to_emit_ansi(logging.StreamHandler.emit)
+    verboselogs.install()
+    logger = logging.getLogger(__name__)
+    os.environ['COLOREDLOGS_LOG_FORMAT'] = FORMAT
+    coloredlogs.install(level='DEBUG', logger=logger)
+    
     chdir = sys.argv[1]
     errors = 0
     errors += run(
